3570c4d16d16d31f3da5ee2ffd9cae87eb2c5fdb
nat <108485997+natalia-the-coder@users.noreply.github.com>
2022-06-30 10:55:48 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ values = [letter for letter in message]
 
 alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
 
-secret_alphabet = [" ", "z", "y", "x", "w", "v", "u", "t", "s", "r", "q", "p", "o", "n", "m", "l", "k", "j", "i", "h", "g", "f", "e", "d", "c", "b", "a"]
+secret_alphabet = ["z", "y", "x", "w", "v", "u", "t", "s", "r", "q", "p", "o", "n", "m", "l", "k", "j", "i", "h", "g", "f", "e", "d", "c", "b", "a", " "]
 
 def encode(values):
   x = ""
